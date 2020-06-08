54b3b802d4244f3eb8e77c224eab9819aa0a2e4f
yxj0312 <yanxiajun@hotmail.com>
2020-06-08 21:40:49 +0200

    Ep6 05:18

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,7 +1,9 @@
-<div>
-    {{ $trigger }}
+<div x-data="{ open: false }" @click.away="open = false">
+    <div @click="open =! open">
+        {{ $trigger }}
+    </div>
 
-    <div class="bg-white rounded shadow-md py-2 mt-1">
+    <div class="bg-white rounded shadow-md py-2 mt-1 w-40" x-show="open">
         {{ $slot }}
     </div>
 </div>
