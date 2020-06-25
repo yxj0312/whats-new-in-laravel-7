477a37ed0e7dcfde1405181bd17e3ae708fba1f6
yxj0312 <yanxiajun@hotmail.com>
2020-06-25 23:02:45 +0200

    Ep8 finished

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,43 +9,45 @@
             <x-nav-link route="contact">Contact</x-nav-link>
         </nav>
 
-        <button class="focus:outline-none" @click="open = ! open">
-            <svg x-show="! open" viewBox="0 0 20 20" width="20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <g stroke="none" stroke-width="1" fill="red" fill-rule="evenodd">
-                    <g fill="white">
-                        <path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z" id="Combined-Shape"></path>
+        <div class="md:hidden">
+            <button class="focus:outline-none" @click="open = ! open">
+                <svg x-show="! open" viewBox="0 0 20 20" width="20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="red" fill-rule="evenodd">
+                        <g fill="white">
+                            <path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z" id="Combined-Shape"></path>
+                        </g>
                     </g>
-                </g>
-            </svg>
+                </svg>
 
-            <svg x-show="open" viewBox="0 0 20 20" width="20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g fill="white">
-                        <polygon id="Combined-Shape" points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
+                <svg x-show="open" viewBox="0 0 20 20" width="20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g fill="white">
+                            <polygon id="Combined-Shape" points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
+                        </g>
                     </g>
-                </g>
-            </svg>
-        </button>
-    </div>
+                </svg>
+            </button>
+        </div>
 
-    <div class="hidden md:block">
-        <x-dropdown alignment="right">
-            <x-slot name="trigger">
-                <button>
-                    <img src="" alt="" class="border-white">123
-                </button>
-            </x-slot>
+        <div class="hidden md:block">
+            <x-dropdown alignment="right">
+                <x-slot name="trigger">
+                    <button>
+                        <img src="" alt="" class="border-white">123
+                    </button>
+                </x-slot>
 
-            <x-dropdown-link href="#" class="text-black">Account</x-dropdown-lin>
-            <x-dropdown-link href="#" class="text-black">Support</x-dropdown-lin>
-        </x-dropdown>
+                <x-dropdown-link href="#" class="text-black">Account</x-dropdown-lin>
+                <x-dropdown-link href="#" class="text-black">Support</x-dropdown-lin>
+            </x-dropdown>
+        </div>
     </div>
 
     {{-- mobile header --}}
-    <div x-show="open" class="mt-3">
+    <nav x-show="open" class="mt-3">
         <x-mobile-nav-link route="home">Home</x-mobile-nav-link>
         <x-mobile-nav-link route="about">About</x-mobile-nav-link>
         <x-mobile-nav-link route="testimonials">Testimonials</x-mobile-nav-link>
         <x-mobile-nav-link route="contact">Contact</x-mobile-nav-link>
-    </div>
+    </nav>
 </header>
