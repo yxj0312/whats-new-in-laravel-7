2577bb09e7b0a163d69400f20f832adab509649a
yxj0312 <yanxiajun@hotmail.com>
2020-06-29 23:11:45 +0200

    10:42

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,10 +1,12 @@
-<div x-data="{ tabHeadings: [] }"
+@props(['active'])
+
+<div x-data="{ activeTab:'', tabHeadings: [] }"
     x-init="() => {
         tabHeadings = [...$refs.tabs.children].map(tab => tab.__x.$data.name);
     }">
     <div>
         <template x-for="(tab, index) in tabHeadings" :key="index">
-            <button x-text="tab"></button>
+            <button x-text="tab" @click="activeTab = tab"></button>
         </template>
     </div>
     <div x-ref="tabs">
