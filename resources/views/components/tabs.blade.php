f0a2e30eff55fd5b2a9d9f962e92e47c558f6877
yxj0312 <yanxiajun@hotmail.com>
2020-06-30 23:28:07 +0200

    12:37

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,6 +1,14 @@
 @props(['active'])
 
-<div x-data="{ activeTab:'', tabHeadings: [] }"
+<div x-data="{ 
+    activeTab:'{{ $active }}', 
+    tabHeadings: [],
+    toggleTabs() {
+        this.tabs.forEach(
+            tab => tab.__x.$data.show = tab.__x.$data.name === this.activeTab
+        );
+    }
+    }"
     x-init="() => {
         tabHeadings = [...$refs.tabs.children].map(tab => tab.__x.$data.name);
     }">
