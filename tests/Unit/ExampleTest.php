9dbff6de2e54db03d0972357712c092b2c468d8e
yxj0312 <yanxiajun@hotmail.com>
2020-08-06 14:29:45 +0200

    A php praxis test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,13 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    function test_echo()
     {
-        $this->assertTrue(true);
+        $a = 10; 
+        
+        dd($a++ + $a++); 
     }
 }
